00c6514147c99fa8fc25ef808064874e44ed9aee
I <jedlikowski.pat@gmail.com>
2018-05-06 21:21:10 +0200

    amount of return edges by edges table

--- Graphs/returnEdges.py
+++ Graphs/returnEdges.py
@@ -16,6 +16,8 @@ def returnEdgesAM(AM, startAndEnd):
             if AM[row - 1][column - 1] == 0: continue               #if there is not connection continue
             if startAndEnd[column][0] < startAndEnd[row][0] and startAndEnd[row][0] < startAndEnd[row][1] and startAndEnd[row][1] < startAndEnd[column][1] :
                 howMany += 1
+            if row == column:      # if there is self connection
+                howMany += 1
     return howMany
         
 #Adjacency List and startAndEnd list
@@ -25,5 +27,16 @@ def returnEdgesAL(Alist, startAndEndList):
         for vertex in connections:
             if startAndEndList[vertex][0] < startAndEndList[index][0] and startAndEndList[index][0] < startAndEndList[index][1] and startAndEndList[index][1] < startAndEndList[vertex][1] :
                 howMany += 1
+            if index == vertex:      # if there is self connection
+                howMany += 1
     return howMany
-    
+
+#Edges Table and startAndEnd list
+def returnEdgesET(ETable, startAndEndList):
+    howMany = 0
+    for connection in ETable:
+        if startAndEndList[connection[1]][0] < startAndEndList[connection[0]][0] and startAndEndList[connection[0]][0] < startAndEndList[connection[0]][1] and startAndEndList[connection[0]][1] < startAndEndList[connection[1]][1] :
+            howMany += 1
+        if connection[0] == connection[1]:      # if there is self connection
+            howMany += 1                     
+    return howMany
